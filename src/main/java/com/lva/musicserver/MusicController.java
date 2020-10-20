f293358ef838ce6c26f008b87ce0ee80c9469d84
laanhca <anhlv98@gmail.com>
2020-10-21 01:52:03 +0700

    a

--- src/main/java/com/lva/musicserver/MusicController.java
+++ src/main/java/com/lva/musicserver/MusicController.java
@@ -14,22 +14,22 @@ import java.util.List;
 @RestController
 public class MusicController {
     @GetMapping("api/searchSong")
-    public List<Musics> searchSong(@RequestParam(value = "musicName", defaultValue = "") String musicName) {
-        ArrayList musics = new ArrayList<Musics>();
-        String link;
-        Elements es = new Elements();
-        if (!musicName.equals("")) { link= "https://chiasenhac.vn/tim-kiem?q="+ musicName.replace(" ","+");
-        try {
-             Document doc = Jsoup.connect(link).get();
-             es= doc.select("div.tab-content").first()
-                     .select("ul.list-unstyled")
-                     .get(1)
-                     .select("li.media");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        } else {
-             link = "https://chiasenhac.vn/nhac-hot.html";
+            public List<Musics> searchSong(@RequestParam(value = "musicName", defaultValue = "") String musicName) {
+                ArrayList musics = new ArrayList<Musics>();
+                String link;
+                Elements es = new Elements();
+                if (!musicName.equals("")) { link= "https://chiasenhac.vn/tim-kiem?q="+ musicName.replace(" ","+");
+                    try {
+                        Document doc = Jsoup.connect(link).get();
+                        es= doc.select("div.tab-content").first()
+                                .select("ul.list-unstyled")
+                                .get(1)
+                                .select("li.media");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    link = "https://chiasenhac.vn/nhac-hot.html";
             try {
                 Document doc = Jsoup.connect(link).get();
 
@@ -40,7 +40,6 @@ public class MusicController {
 
         }
         for (int i=0;i< es.size();i++){
-
                 String linkImage = es.get(i).select("img").first().attr("src");
                 String songName = es.get(i).select("a").attr("title");
                 String artistName = es.get(i).select("div.author").get(0).text();
